faa95a31d696b312e0d9be79388e3f27be2669e3
ZoxDev <mathias.bar1809@gmail.com>
2024-04-10 16:10:09 +0200

    feat: adding searching systeme

--- src/views/RickAndMortyView.tsx
+++ src/views/RickAndMortyView.tsx
@@ -1,20 +1,37 @@
 import { useState } from 'react';
 import { useCharacterPerPages } from '../hooks/useCharacterPerPages';
 import { DataTable } from '../components/DataTable';
+import { TextField } from '@mui/material';
+import { useCharacterPerName } from '../hooks/useCharacterPerName';
+import { useSetDatas } from '../hooks/useSetDatas';
 
 export const RickAndMortyView = () => {
   const [page, setPage] = useState(1);
-  const datas = useCharacterPerPages(page);
+  const allCharacter = useCharacterPerPages(page);
 
-  if (datas.data === undefined) return <h1>Fetching datas...</h1>;
+  const [characterName, setCharactername] = useState('');
+  const searchedCharacter = useCharacterPerName(characterName, page);
+
+  const datas = useSetDatas(characterName, allCharacter?.data, searchedCharacter?.data);
+
+  if (allCharacter.data === undefined || searchedCharacter.data === undefined)
+    return <h1>Fetching datas...</h1>;
+
+  if (datas === undefined) return <h1>Fetching datas...</h1>;
 
   const handleChangePage = (event: unknown, newPage: number) => {
     setPage(newPage);
   };
 
   return (
-    <>
-      <DataTable datas={datas.data} page={page} pageChange={handleChangePage} />
-    </>
+    <main className="flex flex-col p-10">
+      <TextField
+        value={characterName}
+        onChange={(event) => setCharactername(event.target.value)}
+        label="Character name"
+        id="outlined-size-small"
+      />
+      <DataTable datas={datas} page={page} pageChange={handleChangePage} />
+    </main>
   );
 };
